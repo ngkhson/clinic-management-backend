ec6c4475b4bc9707ec77cb2ea3b13df05c131f2f
ngkhson <nkhanhson25112005@gmail.com>
2026-06-24 00:39:49 +0700

    change git account

--- src/main/java/com/clinic/booking/ClinicBookingBackendApplication.java
+++ src/main/java/com/clinic/booking/ClinicBookingBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClinicBookingBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ClinicBookingBackendApplication.class, args);
     }
-
 }
