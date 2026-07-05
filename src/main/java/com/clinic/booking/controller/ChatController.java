64c1d02241e95cc3fe950e630072b872aeca832c
ngkhson <nkhanhson25112005@gmail.com>
2026-07-05 11:20:20 +0700

    update chat controller

--- src/main/java/com/clinic/booking/controller/ChatController.java
+++ src/main/java/com/clinic/booking/controller/ChatController.java
@@ -25,22 +25,23 @@ public class ChatController {
                 chatMessageRequest.getReceiverEmail(),
                 chatMessageRequest.getContent()
         );
-        chatMessageRequest.setTimestamp(savedMessage.getTimestamp());
+
+        savedMessage.setType(chatMessageRequest.getType());
 
         // 2. LOGIC ĐỊNH TUYẾN MỚI (BROADCAST TỚI STAFF)
         if ("STAFF".equals(chatMessageRequest.getReceiverEmail())) {
             // Trường hợp 1: Bệnh nhân gửi tin nhắn cho phòng khám
             // -> Phát tin nhắn lên kênh chung của nhân viên (/topic/staff)
-            messagingTemplate.convertAndSend("/topic/staff", chatMessageRequest);
+            messagingTemplate.convertAndSend("/topic/staff", savedMessage);
             // -> Gửi lại một bản copy cho bệnh nhân để hiển thị trên màn hình của họ
-            messagingTemplate.convertAndSend("/queue/messages/" + chatMessageRequest.getSenderEmail(), chatMessageRequest);
+            messagingTemplate.convertAndSend("/queue/messages/" + chatMessageRequest.getSenderEmail(), savedMessage);
         } else {
             // Trường hợp 2: Nhân viên (Admin/Doctor) trả lời bệnh nhân
             // -> Gửi trực tiếp đến kênh riêng của bệnh nhân đó
-            messagingTemplate.convertAndSend("/queue/messages/" + chatMessageRequest.getReceiverEmail(), chatMessageRequest);
+            messagingTemplate.convertAndSend("/queue/messages/" + chatMessageRequest.getReceiverEmail(), savedMessage);
             // -> Đồng thời phát lại tin nhắn này lên kênh chung của nhân viên
             // (để các Admin/Doctor khác thấy câu trả lời và biết khách đã được hỗ trợ)
-            messagingTemplate.convertAndSend("/topic/staff", chatMessageRequest);
+            messagingTemplate.convertAndSend("/topic/staff", savedMessage);
         }
     }
 }
